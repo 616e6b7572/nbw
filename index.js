0868a2e98b6a8c78ab1bbdb6482619f4f3666d4a
616e6b7572 <616e6b7572@gmail.com>
2019-06-21 19:01:39 +0530

    beta

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
-
 const fs = require('fs');
-const path = require('path');
 const os = require('os');
+const { execSync } = require('child_process');
 const { exec } = require('child_process');
 const util = require('util');
-const SDIR = os.homedir()+'/.g_node_modules';
+const SDIR = os.homedir()+'/.nbw';
+
+function getProcess(){
+	// eslint-disable-next-line no-undef
+	return process;
+}
+
+function getConsole(){
+	// eslint-disable-next-line complexity
+	return console;
+}
+
 
 class BaseExec{
 	constructor(){
@@ -13,32 +23,39 @@ class BaseExec{
 	}
 	execute(cmd){
 		exec(cmd, (error, stdout, stderr) => {
-		  if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		  }
-		  console.log(`${stdout}`);
-		  console.log(`${stderr}`);
+			if (error) {
+				getConsole().error(`exec error: ${error}`);
+				return;
+			}
+			getProcess().stdout.write(`${stdout}`);
+			getConsole().error(`${stderr}`);
 		});
+	}
+	executeSync(cmd){
+		var r = execSync(cmd);
+		getConsole().log(r);
 	}
 	execFile(cmd, args){
 		const execFile = util.promisify(require('child_process').execFile);
 		async function getVersion(cmd, args) {
-		const { stdout } = await execFile(cmd, args, (error, stdout, stderr) => {
-			if (error) {
-				console.error(`exec error: ${error}`);
-				return;
-			}
-			console.log(`${stdout}`);
-			console.log(`${stderr}`);
-		}).catch((e) => {console.log(e)});
-			console.log(stdout);
+			const { stdout } = await execFile(cmd, args, (error, stdout, stderr) => {
+				if (error) {
+					getConsole().error(`exec error: ${error}`);
+					return;
+				}
+				stdout = stdout.replace('npm', 'nbw');
+				stderr = stderr.replace('npm', 'nbw');
+				getProcess().stdout.write(`${stdout}`);
+				getConsole().error(`${stderr}`);
+			}).catch((e) => {getProcess().stdout.write(e);});
+			getProcess().stdout.write(stdout);
 		} 
 		getVersion(cmd, args);	
 	}
 	fork(cmd, args){
-		const cp = require('child_process');
+		const cp = require('child_getProcess()');
 		const n = cp.fork(cmd, args);
+		return n;
 	}
 }
 
@@ -46,27 +63,27 @@ class Exec extends BaseExec{
 	constructor(){
 		super();
 		try{
-			var s = fs.openSync('./package.json', 'r');
+			fs.openSync('./package.json', 'r');
 			try{
-				var s = fs.openSync('cd '+SDIR, 'r');
+				fs.openSync('cd '+SDIR, 'r');
 			}
 			catch(e){
 				Boot.execute();
 			}
 		}
 		catch(e){
-			process.stdout.write(`ERROR: File "package.json" not available.\n Try: npm init [-f|--force|-y|--yes] \n`);
-			process.exit();
+			getProcess().stdout.write('ERROR: File "package.json" not available.\n Try: npm init [-f|--force|-y|--yes] \n');
+			getProcess().exit();
 		}
 	}
 }
 
-class Boot extends Exec{
+class Boot extends BaseExec{
 	constructor(){
 		super();	
 	}
 	static getNPM(){
-		var path = process.env.PATH.split(':');
+		var path = getProcess().env.PATH.split(':');
 		var npm = '';
 		var i = 0;
 		for(i in path){
@@ -77,47 +94,52 @@ class Boot extends Exec{
 				break;
 			}
 			catch(e){
-				
+				getProcess().stdout.write(`${e}`);
 			}
 		}
 		if(npm == ''){
-			process.stdout.write(`ERROR: Unable  to find "npm", Please make sure it's in your ENV PATH. \n`);
-			process.exit();	
+			getProcess().stdout.write('ERROR: Unable  to find "npm", Please make sure it\'s in your ENV PATH. \n');
+			getProcess().exit();	
 		}
 		return npm;
 	}
 	static execute(){
-		fs.stat(SDIR, (err, stats) => {
-			if (err){
-				if(err.code == 'ENOENT'){
-					fs.mkdir(SDIR, (err) => {
-						if (err) throw err;
-						fs.stat(SDIR+'/node_modules', (err, stats) => {
-							if (err){
-								if(err.code == 'ENOENT'){
-									fs.mkdir(SDIR+'/node_modules', (err) => {
-									if (err) throw err;
-
-									});
-								}
-							}
-						});
-					
-						fs.writeFile(SDIR+'/package.json'
-							, '{\n"name": "g_node_modules",\n"version": "1.0.0",\n"description": "npm",\n"main": "index.js",\n"scripts": {\n\t"test": "echo \\"Error: no test specified\\" && exit 1"\n},\n"repository": {\n\t"type": "git",\n\t"url": "git+https://github.com/"\n},\n"keywords": [],\n"author": "",\n"license": "ISC"\n}'
-							, 'utf8', 
-							function(err){if(err) throw err;}
-						);
-					
-						fs.writeFile(SDIR+'/index.js'
-							,''
-							, 'utf8', 
-							function(err){if(err) throw err;}
-						);
-					});
-				}  
+		try{
+			fs.statSync(SDIR);
+			try{
+				fs.statSync('./node_modules');
 			}
-		});
+			catch(err){
+				if(err.code == 'ENOENT'){
+					fs.symlinkSync(SDIR+'/node_modules', 'node_modules');
+				}
+			}
+		}
+		catch(err){
+			if(err.code == 'ENOENT'){
+				try{
+					fs.mkdirSync(SDIR);
+					fs.mkdirSync(SDIR+'/node_modules');
+					fs.symlinkSync(SDIR+'/node_modules', 'node_modules');
+
+					fs.writeFile(SDIR+'/package.json'
+						, '{\n"name": "_nbw",\n"version": "1.0.0",\n"description": "npm",\n"main": "index.js",\n"scripts": {\n\t"test": "echo \\"Error: no test specified\\" && exit 1"\n},\n"repository": {\n\t"type": "git",\n\t"url": "git+https://github.com/"\n},\n"keywords": [],\n"author": "",\n"license": "ISC"\n}'
+						, 'utf8', 
+						function(err){if(err) throw err;}
+					);
+				
+					fs.writeFile(SDIR+'/index.js'
+						,''
+						, 'utf8', 
+						function(err){if(err) throw err;}
+					);
+				}
+				catch(e){
+					getProcess().stdout.write('ERROR: Unable to create global node_modules folder. Please try again! \n');
+					getProcess().exit();
+				}	
+			}
+		}
 	}
 }
 
@@ -127,9 +149,38 @@ class Install extends Exec{
 		this.argv = argv;		
 	}
 	execute(){
-		super.execute('cd '+SDIR+' && npm '+this.argv.join(' '));
+		Boot.execute();
+		
+		var flags = [];
+		
+		for(let i in this.argv){
+			if(this.argv[i].search('--') > -1){
+				flags.push(this.argv[i]);
+			}
+		}
+		
+		super.executeSync('cd '+SDIR+' && npm '+this.argv.join(' '));
+		
+		this.postInstall(flags);
+		
 		//super.execute('npm ');
 	}
+	postInstall(flags){
+		
+		
+		fs.readFile('./package.json', 'utf8', function readFileCallback(err, data){
+			if (err){
+				getConsole().log(err);
+			} else {
+				
+				flags = JSON.parse(datas);
+				//getConsole().log(flags['devDependencies']);
+				//flags = JSON.stringify(flags);
+				//fs.writeFileSync('package.json', flags, 'utf8');
+			}
+		});
+	}
+	
 }
 
 class Init extends BaseExec{
@@ -140,43 +191,40 @@ class Init extends BaseExec{
 	execute(){
 		var npm = Boot.getNPM();
 		super.fork(`${npm}`, this.argv);
-		fs.symlinkSync(SDIR, 'node_modules');
 	}
 }
 
-class NPM extends Exec{
+class NPM extends BaseExec{
 	constructor(argv){
 		super();
 		this.argv = argv;
 	}
 	execute(){
-		var npm = Boot.getNPM();
-		super.fork(`${npm}`, this.argv);
+		var cmd = 'npm';
+		var args = this.argv;
+		if(args.length < 1){args.push('help');}
+		super.execFile(cmd, args);
 	}
 }
 
 const CLI = function(){
 	var argv = [];
-	process.argv.forEach((val, index) => {
-	  if(index > 1){argv.push(val);}
+	getProcess().argv.forEach((val, index) => {
+		if(index > 1){argv.push(val);}
 	});
-	this.boot = function(){		
-		//Boot.execute();
-	}
 	this.install = function(argv){
 		var install = new Install(argv); 
 		install.execute();
-	}
+	};
 	this.init = function(argv){
 		var init = new Init(argv); 
 		init.execute();
-	}
+	};
 	this.npm = function(argv){
 		var npm = new NPM(argv); 
 		npm.execute();
-	}
+	};
 	this.execute = function(){
-		this.boot();
 		if(this[argv[0]] != undefined){
 			this[argv[0]](argv);
 		}
@@ -184,7 +232,7 @@ const CLI = function(){
 			this.npm(argv);
 		}
 	};
-}
+};
 
 const nbw = function(){
 	var cli = new CLI();
